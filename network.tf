ec3d4971c6e9da803f8573e9ecdd58eeeb66ded3
cperson-palo <72570064+cperson-palo@users.noreply.github.com>
2021-02-26 16:09:12 -0500

    Update network.tf
    Adjusted for Panorama - only one interface required

--- network.tf
+++ network.tf
@@ -1,18 +1,16 @@
 ##############################################################################
 # This file creates reads network resources for the solution.
-#  - Subnet ID for Firewall Management
-#  - Subnet ID for Firewall Untrust interface
-#  - Subnet ID for Firewall Trust interface
+#  - Subnet ID for Panorama Management
 ##############################################################################
 
 data "ibm_is_subnet" "vnf_subnet1"{
    identifier = var.subnet_id1
 }
 
-data "ibm_is_subnet" "vnf_subnet2"{
-   identifier = var.subnet_id2
-}
-
-data "ibm_is_subnet" "vnf_subnet3"{
-   identifier = var.subnet_id3
-}
+#data "ibm_is_subnet" "vnf_subnet2"{
+#   identifier = var.subnet_id2
+#}
+#
+#data "ibm_is_subnet" "vnf_subnet3"{
+#   identifier = var.subnet_id3
+#}
