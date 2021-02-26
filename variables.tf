b81503f53f042644324e2e2ce91864a69f1ebcc3
cperson-palo <72570064+cperson-palo@users.noreply.github.com>
2021-02-26 16:18:32 -0500

    Update variables.tf
    Updated for Panorama
    Made cos_image live
    Commented out image_name
    Commented out subnet_id2 and id3

--- variables.tf
+++ variables.tf
@@ -2,63 +2,63 @@
 # Variable block - See each variable description
 ##############################################################################
 
+###############################################################################
+# vnf_cos_image_url - Vendor provided Panorama qcow2 Image COS url.
 ##############################################################################
-# vnf_cos_image_url - Vendor provided VM-Series qcow2 Image COS url.
-##############################################################################
-# variable "vnf_cos_image_url" {
-#  description = "The COS image object SQL URL for VM-Series qcow2 image."
-#}
-##############################################################################
-# image_name - The name of the Palo Alto VM-Series (PanOS) image . Valid values are "pa-vm-kvm-9-1-3-1"
-##############################################################################
-variable "image_name" {
-  default     = "pa-vm-kvm-9-1-3-1"
-  description = "The name of the Palo Alto VM-Series (PanOS) image. Valid values are 'pa-vm-kvm-9-1-3-1"
+ variable "vnf_cos_image_url" {
+  description = "The COS image object SQL URL for Panorama qcow2 image."
 }
+##############################################################################
+# image_name - The name of the Palo Alto Panorama image . Valid values are "pa-vm-kvm-9-1-3-1"
+##############################################################################
+#variable "image_name" {
+#  default     = "pa-vm-kvm-9-1-3-1"
+#  description = "The name of the Palo Alto Panorama (PanOS) image. Valid values are 'pa-vm-kvm-9-1-3-1"
+#}
 ##############################################################################
 # subnet_ids - Subnet where resources are to be provisioned.
 ##############################################################################
 variable "subnet_id1"{
-  description =  "The id of the subnet to which VM-Series VSI's first interface(management) belongs to"
+  description =  "The id of the subnet to which Panorama VSI's first interface(management) belongs to"
 }
 
-variable "subnet_id2"{
-  description = "The id of the subnet to which VM-Series VSI's second interface(untrust) belongs to"
-}
+#variable "subnet_id2"{
+#  description = "The id of the subnet to which Panorama VSI's second interface(untrust) belongs to"
+#}
 
-variable "subnet_id3"{
-  description = "The id of the subnet to which VM-Series VSI's third interface(trust) belongs to"
-}
+#variable "subnet_id3"{
+#  description = "The id of the subnet to which Panorama VSI's third interface(trust) belongs to"
+#}
 
 ##############################################################################
-# ssh_key_name - The name of the public SSH key to be used when provisining VM-Series VSI.
+# ssh_key_name - The name of the public SSH key to be used when provisining Panorama VSI.
 ##############################################################################
 variable "ssh_key_name" {
-  description = "The name of the public SSH key to be used when provisining VM-Series VSI."
+  description = "The name of the public SSH key to be used when provisining Panorama VSI."
 }
 
 ##############################################################################
-# vnf_vpc_image_name - The name of the VM-Series custom image to be provisioned in your IBM Cloud account.
+# vnf_vpc_image_name - The name of the Panorama custom image to be provisioned in your IBM Cloud account.
 ##############################################################################
 variable "vnf_vpc_image_name" {
-  default     = "vm-series-byol"
-  description = "The name of the VM-Series custom image to be provisioned in your IBM Cloud account (lowercase)"
+  default     = "panorama-byol"
+  description = "The name of the Panorama custom image to be provisioned in your IBM Cloud account (lowercase)"
 }
 
 ##############################################################################
-# vnf_vpc_image_name - The name of your VM-Series Virtual Server to be provisioned
+# vnf_vpc_image_name - The name of your Panorama Virtual Server to be provisioned
 ##############################################################################
 variable "vnf_instance_name" {
-  default     = "vm-series-byol-vsi"
-  description = "The name of your VM-Series Virtual Server to be provisioned."
+  default     = "panorama-byol-vsi"
+  description = "The name of your Panorama Virtual Server to be provisioned."
 }
 
 ##############################################################################
-# vnf_profile - The profile of compute CPU and memory resources to be used when provisioning VM-Series VSI.
+# vnf_profile - The profile of compute CPU and memory resources to be used when provisioning Panorama VSI.
 ##############################################################################
 variable "vnf_profile" {
   default     = "bx2-8x32"
-  description = "The profile of compute CPU and memory resources to be used when provisioning VM-Series VSI. To list available profiles, run `ibmcloud is instance-profiles`."
+  description = "The profile of compute CPU and memory resources to be used when provisioning Panorama VSI. To list available profiles, run `ibmcloud is instance-profiles`."
 }
 
 variable "region" {
